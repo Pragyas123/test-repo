713eeba27a04771403c69cec3074e0981189df81
Pragyas123 <pragyas@mediawareonline.com>
2023-03-19 23:03:18 +0530

    modified new

--- src/test/myclass.java
+++ src/test/myclass.java
@@ -4,6 +4,7 @@ public class myclass {public static void main(String []args) {
 
 	System.out.println("Hello world");
 	System.out.println("printing new msg");
+	System.out.println("printing new msg2");
 }
 	
 
